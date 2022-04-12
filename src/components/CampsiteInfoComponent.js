81146ee27217c91940578dbf9ba00ffca3e39566
Ben Jacobs <bencjacobs6@gmail.com>
2022-04-12 15:42:53 -0400

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { Comments } from '../shared/campsites';
+
 
 class CampsiteInfo extends Component {
 
@@ -39,9 +39,11 @@ class CampsiteInfo extends Component {
     render() {
         if (this.props.campsite) {
             return (
-                <div className='row'>
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className='container'>
+                    <div className='row'>
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             )
         }
