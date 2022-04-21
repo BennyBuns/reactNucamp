6c7adb098bd97f54e68a55905868b7ddb70e4fb7
Ben Jacobs <bencjacobs6@gmail.com>
2022-04-21 16:33:03 -0400

    Controlled Form Validation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,7 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import About from './AboutComponent'
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
