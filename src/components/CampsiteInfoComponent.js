fb445f99412268201813ae156a1fca1e0f270f05
Ben Jacobs <bencjacobs6@gmail.com>
2022-04-30 21:29:33 -0400

    workshop assignment 5

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger, FadeTransform } from 'react-animation-components';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -12,12 +13,16 @@ const minLength = len => val => val && (val.length >= len);
 
 function RenderCampsite({ campsite }) {
     return (<div className='col-md-5 m-1'>
-        <Card>
-            <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
-            <CardBody>
-                <CardText>{campsite.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform in transformProps={{
+            exitTransform: 'scale(0.5) translateY(-50%)'
+        }}>
+            <Card>
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+                <CardBody>
+                    <CardText>{campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     </div>
     );
 }
@@ -27,15 +32,20 @@ function RenderComments({ comments, postComment, campsiteId }) {
         return (
             <div className='col-md-5 m-1'>
                 <h4>Comments</h4>
-                {comments.map(comment => {
-                    return (<div key={comment.id}>
-                        <p>{comment.text} <br />
-                            -- {comment.author},
-                            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
-                        </p>
-                    </div>
-                    );
-                })}
+                <Stagger in>
+                    {comments.map(comment => {
+                        return (
+                            <Fade in key={comment.id}>
+                                <div >
+                                    <p>{comment.text} <br />
+                                        -- {comment.author},
+                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                                </div>
+                            </Fade>
+                        );
+                    })}
+                </Stagger>
                 <CommentForm campsiteId={campsiteId} postComment={postComment} />
             </div>
         );
@@ -79,10 +89,10 @@ function CampsiteInfo(props) {
                 </div>
                 <div className='row'>
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
-                    comments={props.comments} 
-                    postComment={props.postComment}
-                    campsiteId={props.campsite.id}
+                    <RenderComments
+                        comments={props.comments}
+                        postComment={props.postComment}
+                        campsiteId={props.campsite.id}
                     />
                 </div>
             </div>
@@ -118,7 +128,7 @@ class CommentForm extends Component {
     handleSubmit(values) {
         this.toggleModal();
         this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
-        
+
     }
 
     render() {
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Loading } from './LoadingComponent';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform } from 'react-animation-components';
 
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
@@ -12,13 +13,19 @@ function RenderCard({ item, isLoading, errMess }) {
     }
 
     return (
-        <Card>
-            <CardImg src={baseUrl + item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
+        <FadeTransform
+            in
+            transformProps={{
+                exitTransform: 'scale(0.5) translateY(50%)'
+            }}>
+            <Card>
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        </FadeTransform>
     );
 }
 
@@ -34,14 +41,18 @@ function Home(props) {
                     />
                 </div>
                 <div className='col-md m-1'>
-                    <RenderCard 
-                        item={props.promotion} 
+                    <RenderCard
+                        item={props.promotion}
                         isLoading={props.promotionLoading}
                         errMess={props.promotionErrMess}
                     />
                 </div>
                 <div className='col-md m-1'>
-                    <RenderCard item={props.partner} />
+                    <RenderCard
+                        item={props.partner}
+                        isLoading={props.partnerLoading}
+                        errMess={props.partnerErrMess}
+                    />
                 </div>
             </div>
         </div>
